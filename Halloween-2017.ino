f44e592f6e757b2c35327df56d0dd609865973bf
Enrique Crespo <aprendiendoarduino@gmail.com>
2017-11-04 09:18:44 +0100

    Added lcd display and change some var names

--- Halloween-2017.ino
+++ Halloween-2017.ino
@@ -1,36 +1,46 @@
 #include "LowPower.h"
 #include "FastLED.h"
+#include <SoftwareSerial.h> //For LCD management
 
-// How many leds in your strip?
-#define NUM_LEDS 12
-
+#define NUM_LEDS 12 // How many leds in your strip
 #define DATA_PIN 4
 #define DATA_PIN2 9
+#define WAKEUP_PIN 2  //Wakeup pin is connected to Infrared line track sensor digital output
+#define BUZZER_PIN 8
 
 // Define the array of leds
 CRGB leds[NUM_LEDS];
 CRGB leds2[NUM_LEDS];
 
-// Use pin 2 as wake up pin
-const int wakeUpPin = 2;
+int candy_counter = 0;
 
-const int zumbador = 8; // Pin digital para el zumbador
-
-void wakeUp()
-{
-  // Just a handler for the pin interrupt.
-}
+// Attach the serial display's RX line to digital pin 6
+SoftwareSerial mySerial(7, 6); // pin 6 = TX, pin 7 = RX (unused)
 
 void setup()
 {
   // Configure wake up pin as input.
   // This will consumes few uA of current.
-  pinMode(wakeUpPin, INPUT_PULLUP);
-  pinMode(zumbador, OUTPUT); // Pin digital 8 como salida
+  pinMode(WAKEUP_PIN, INPUT_PULLUP);
+  pinMode(BUZZER_PIN, OUTPUT); // Pin digital 8 como salida
   Serial.begin(9600);
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
   FastLED.addLeds<NEOPIXEL, DATA_PIN2>(leds2, NUM_LEDS);
   LEDS.setBrightness(84);
+  mySerial.begin(9600); // set up serial port for 9600 baud
+  delay(500); // wait for display to boot up
+  mySerial.write(254); // move cursor to beginning of first line
+  mySerial.write(128);
+  mySerial.write("                "); // clear display
+  mySerial.write(254); // move cursor to beginning of first line
+  mySerial.write(192);
+  mySerial.write("                "); // clear display
+  mySerial.write(254); // move cursor to beginning of first line
+  mySerial.write(128);
+  mySerial.print("Number Candies:");
+  mySerial.write(254); // move cursor to beginning of second line
+  mySerial.write(192);
+  mySerial.print(candy_counter);
 }
 
 void loop()
@@ -51,7 +61,11 @@ void loop()
 }
 
 void efectos () {
-  digitalWrite(zumbador, HIGH);
+  candy_counter ++;
+  mySerial.write(254); // move cursor to beginning of second line
+  mySerial.write(192);
+  mySerial.print(candy_counter);
+  digitalWrite(BUZZER_PIN, HIGH);
   for (int j = 0; j < 10; j++) {
     for (int i = 0; i < NUM_LEDS; i++) {
       leds[i] = CRGB::Red;
@@ -63,9 +77,12 @@ void efectos () {
       FastLED.show();
       delay(5);
     }
-    digitalWrite(zumbador, !digitalRead(zumbador));
+    digitalWrite(BUZZER_PIN, !digitalRead(BUZZER_PIN));
   }
-  digitalWrite(zumbador, LOW);
+  digitalWrite(BUZZER_PIN, LOW);
 }
 
-
+void wakeUp()
+{
+  // Just a handler for the pin interrupt.
+}
